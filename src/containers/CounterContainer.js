8ac1ee16f5d9fb51105ea2898d46053bf99133bb
guswns1659 <guswns1659@gmail.com>
2022-02-16 21:11:10 +0900

    feat: Apply prettier

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,28 +1,29 @@
-import {connect} from 'react-redux'
-import Counter from "../components/Counter";
-import {decrease, increase} from "../modules/counter";
-import {bindActionCreators} from "redux";
+import { connect } from 'react-redux';
+import Counter from '../components/Counter';
+import { decrease, increase } from '../modules/counter';
 
-const CounterContainer = ({number, increase, decrease}) => {
-    return <Counter number={number} onIncrease={increase} onDecrease={decrease}/>
-}
+const CounterContainer = ({ number, increase, decrease }) => {
+  return <Counter number={number} onIncrease={increase} onDecrease={decrease} />;
+};
 
 const mapStateToProps = state => ({
-    number: state.counter.number,
-})
+  number: state.counter.number,
+});
 
 const mapDispatchToProps = dispatch => ({
-    increase: () => {
-        dispatch(increase());
+  increase: () => {
+    dispatch(increase());
+  },
+  decrease:
+    () => {
+      dispatch(decrease());
     },
-    decrease: () => {
-        dispatch(decrease());
-    }
-})
+});
 
-export default connect(state => ({
-    number: state.counter.number
-}), dispatch => bindActionCreators({
+export default connect(
+  state => ({
+    number: state.counter.number,
+  }), {
     increase,
-    decrease
-}, dispatch))(CounterContainer);
+    decrease,
+  })(CounterContainer);
