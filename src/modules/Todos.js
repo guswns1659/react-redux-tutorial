200c21f1c301a800ca9f6a96d5361adac8d5a874
guswns1659 <guswns1659@gmail.com>
2022-02-17 06:50:19 +0900

    redux refactor: Use immer in todos reducer

--- src/modules/Todos.js
+++ src/modules/Todos.js
@@ -1,5 +1,6 @@
 // action type
 import { createAction, handleActions } from 'redux-actions';
+import produce from 'immer';
 
 const CHANGE_INPUT = 'todos/CHANGE_INPUT';
 const INSERT = 'todos/INSERT';
@@ -41,10 +42,11 @@ const todos = handleActions(
   {
     [CHANGE_INPUT]: (state, { payload: input }) => ({ ...state, input: input }),
     [INSERT]: (state, { payload: todo }) => ({ ...state, todos: state.todos.concat(todo) }),
-    [TOGGLE]: (state, { payload: id }) => ({
-      ...state, todos: state.todos.map(todo =>
-        todo.id === id ? { ...todo, done: !todo.done } : todo),
-    }),
+    [TOGGLE]: (state, { payload: id }) =>
+      produce(state, draft => {
+        const todo = draft.todos.find(todo => todo.id === id);
+        todo.done = !todo.done;
+      }),
     [REMOVE]: (state, { payload: id }) => ({ ...state, todos: state.todos.filter(todo => todo.id !== id) }),
   },
   initialState,
