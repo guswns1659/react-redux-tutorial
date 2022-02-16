b4e6c8462603ceb6419010318f57b0373137c10c
guswns1659 <guswns1659@gmail.com>
2022-02-16 13:40:17 +0900

    feat: Add counter, todos redux modules

---
+++ src/modules/Todos.js
@@ -0,0 +1,78 @@
+// action type
+const CHANGE_INPUT = 'todos/CHANGE_INPUT';
+const INSERT = 'todos/INSERT';
+const TOGGLE = 'todos/TOGGLE';
+const REMOVE = 'todos/REMOVE';
+
+// action create func
+export const changeInput = input => ({
+    type: CHANGE_INPUT,
+    input
+});
+
+let id = 3;
+export const insert = text => ({
+    type: INSERT,
+    todo: {
+        id: id++,
+        text,
+        done: false
+    }
+});
+
+export const toggle = id => ({
+    type: TOGGLE,
+    id
+});
+
+export const remove = id => ({
+    type: REMOVE,
+    id
+});
+
+// reducer
+const initialState = {
+    input: '',
+    todos: [
+        {
+            id: 1,
+            text: 'Redux basic',
+            done: true
+        },
+        {
+            id: 2,
+            text: 'React & React Practice',
+            done: false
+        }
+    ]
+}
+
+function todos(state=initialState, action) {
+    switch (action.type) {
+        case CHANGE_INPUT:
+            return {
+                ...state,
+                input: action.input
+            }
+        case INSERT:
+            return {
+                ...state,
+                todos: state.todos.concat(action.todo)
+            }
+        case TOGGLE:
+            return {
+                ...state,
+                todos: state.todos.map(todo =>
+                todo.id === action.id ? {...todo, done: !todo.done} : todo)
+            }
+        case REMOVE:
+            return {
+                ...state,
+                todos: state.todos.filter(todo => todo.id !== action.id)
+            }
+        default:
+            return state;
+    }
+}
+
+export default todos;
