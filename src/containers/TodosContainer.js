b5d589dbfefccc7026a4f5ba75cf60acd3e9d901
guswns1659 <guswns1659@gmail.com>
2022-02-17 07:20:28 +0900

    redux refactor: refactor TodosContainer with react hooks

--- src/containers/TodosContainer.js
+++ src/containers/TodosContainer.js
@@ -1,32 +1,27 @@
 import Todos from '../components/Todos';
+import { useDispatch, useSelector } from 'react-redux';
+import { useCallback } from 'react';
 import { changeInput, insert, remove, toggle } from '../modules/Todos';
-import { connect } from 'react-redux';
 
-const TodosContainer = ({
-                          input,
-                          todos,
-                          changeInput,
-                          insert,
-                          toggle,
-                          remove,
-                        }) => {
-  return <Todos input={input}
-                todos={todos}
-                onChangeInput={changeInput}
-                onInsert={insert}
-                onToggle={toggle}
-                onRemove={remove} />;
-};
-
-export default connect(
-  ({ todos }) => ({
+const TodosContainer = () => {
+  const { input, todos } = useSelector(({ todos }) => ({
     input: todos.input,
     todos: todos.todos,
-  }),
-  {
-    changeInput,
-    insert,
-    toggle,
-    remove,
-  },
-)(TodosContainer);
+  }));
+
+  const dispatch = useDispatch();
+
+  const onChangeInput = useCallback(input => dispatch(changeInput(input)), [dispatch])
+  const onInsert = useCallback(text => dispatch(insert(text)), [dispatch])
+  const onToggle = useCallback(id => dispatch(toggle(id)), [dispatch])
+  const onRemove = useCallback(id => dispatch(remove(id)), [dispatch])
+
+  return <Todos input={input}
+                todos={todos}
+                onChangeInput={onChangeInput}
+                onInsert={onInsert}
+                onToggle={onToggle}
+                onRemove={onRemove} />;
+};
+
+export default TodosContainer;
