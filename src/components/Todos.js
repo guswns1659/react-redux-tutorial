411e819314620dfc34640b043e8abfec5f04c935
guswns1659 <guswns1659@gmail.com>
2022-02-18 07:29:43 +0900

    redux repeat: Create Counter, Todos components

---
+++ src/components/Todos.js
@@ -0,0 +1,35 @@
+const TodoItem = ({ todo, onToggle, onRemove }) => {
+  return (
+    <div>
+      <input type='checkbox' />
+      <span>Example text</span>
+      <button>Remove</button>
+    </div>
+  );
+};
+
+
+const Todos = ({ input, todos, onChangeInput, onInsert, onToggle, onRemove }) => {
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input />
+        <button type='submit'>Register</button>
+      </form>
+      <div>
+        <TodoItem></TodoItem>
+        <TodoItem></TodoItem>
+        <TodoItem></TodoItem>
+        <TodoItem></TodoItem>
+        <TodoItem></TodoItem>
+      </div>
+    </div>
+  );
+};
+
+export default Todos;
