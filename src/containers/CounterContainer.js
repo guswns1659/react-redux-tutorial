1f940dc0f6a295a3211605836d56e6b844cbdaa9
guswns1659 <guswns1659@gmail.com>
2022-02-28 06:39:37 +0900

    redux repeat: Using useCallback for improving performance

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,13 +1,18 @@
 import Counter from '../components/Counter';
 import { useDispatch, useSelector } from 'react-redux';
 import { decrease, increase } from '../modules/counter';
+import { useCallback } from 'react';
 
 const CounterContainer = () => {
   const number = useSelector(state => state.counter.number);
   const dispatch = useDispatch();
+  const onIncrease = useCallback(() => dispatch(increase()), [dispatch]); //
+  const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
 
   return (
-    <Counter number={number} onIncrease={() => dispatch(increase())} onDecrease={() => dispatch(decrease())}></Counter>
+    <Counter number={number}
+             onIncrease={onIncrease}
+             onDecrease={onDecrease}></Counter>
   )
 }
 
